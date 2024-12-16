93963d2b438367807a1112f07dd20c6c9d24005a
AndRBuZ <andrrrw56@gmail.com>
2024-12-16 13:48:16 +0000

    Change type of column price and add steam_id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_03_123055) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_12_181245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "game_marketplaces", force: :cascade do |t|
-    t.integer "price", null: false
+    t.string "price", null: false
     t.string "rating"
     t.bigint "game_id", null: false
     t.bigint "marketplace_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "steam_id"
     t.index ["game_id"], name: "index_game_marketplaces_on_game_id"
     t.index ["marketplace_id"], name: "index_game_marketplaces_on_marketplace_id"
   end
