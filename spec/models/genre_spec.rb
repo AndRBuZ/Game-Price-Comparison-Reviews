20d7830ad36124f0f6cc71cd98d6d13a07ee6db8
AndRBuZ <andrrrw56@gmail.com>
2024-12-17 14:25:30 +0000

    test: refactor and add missing tests

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -2,21 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
   describe 'Validations' do
-    context 'with valid attributes' do
-      let(:genre) { build(:genre) }
-
-      it 'is valid' do
-        expect(genre).to be_valid
-      end
-    end
-
-    context 'with invalid attributes' do
-      it 'is invalid without a name' do
-        genre = build(:genre, name: nil)
-        expect(genre).to_not be_valid
-        expect(genre.errors[:name]).to include("can't be blank")
-      end
-    end
+    it { should validate_presence_of(:name) }
   end
 
   describe "Associations" do
