4a95afac5f96029f5bf5685c94a62fced52fbf55
AndRBuZ <andrrrw56@gmail.com>
2025-01-09 18:05:04 +0000

    feat: add email format validation to User model
    - Introduced a regular expression to validate the format of user email addresses in the User model.
    - Updated user_spec to include tests for valid and invalid email formats, ensuring proper validation behavior.
    - This enhancement improves data integrity by ensuring only properly formatted email addresses are accepted.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe User, type: :model do
 
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should allow_value('test@example.com').for(:email) }
+    it { should_not allow_value('test@example').for(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:nickname) }
   end
