3d513f3ba7154180d47fdba482502a2845f83ca8
AndRBuZ <andrrrw56@gmail.com>
2025-01-14 17:07:32 +0000

    fix: improve data handling and validations in game marketplace and job services
    - Updated SteamUpdatePricesJob to skip nil steam IDs when updating prices, ensuring only valid IDs are processed.
    - Modified GameMarketplace model validations to allow nil values for steam_id and xbox_id, enhancing flexibility in data entry.
    - Refactored GameDataSaver to maintain consistent attribute assignment and error handling during game record creation.
    
    These changes enhance data integrity and prevent potential errors during price updates and game data saving.

--- app/jobs/steam_update_prices_job.rb
+++ app/jobs/steam_update_prices_job.rb
@@ -2,6 +2,6 @@ class SteamUpdatePricesJob < ApplicationJob
   queue_as :default
 
   def perform
-    GameMarketplace.pluck(:steam_id).each { |steam_id| SteamApiService.new(steam_id).update_price }
+    GameMarketplace.pluck(:steam_id).compact.each { |steam_id| SteamApiService.new(steam_id).update_price }
   end
 end
--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
   describe 'Validations' do
+    subject { Genre.new(name: "Action") }
+
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
   end
